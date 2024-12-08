35b3c023b7e5355dd0b39712825d35b746fc0574
TiruRepo <111552420+TiruRepo@users.noreply.github.com>
2024-12-08 13:11:48 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a0002747a72e64c1797680dc807c946b-1367617675.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a4392e79394474a938d5a339187abb1b-425519001.ap-south-1.elb.amazonaws.com',
 };
